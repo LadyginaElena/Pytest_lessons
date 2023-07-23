96e28e7e97ede4c6be5f453810e46dd36c5a7715
Elena Ladygina <ladygina.ev@mail.ru>
2023-07-23 10:03:35 +0700

    changed conftest.py all tests passed

--- conftest.py
+++ conftest.py
@@ -1,27 +1,14 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-@pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        service = Service(executable_path=ChromeDriverManager().install())
-        browser = webdriver.Chrome(service=service)
-        browser.implicitly_wait(15)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        service = Service(executable_path=GeckoDriverManager().install())
-        browser = webdriver.Firefox(service=service)
-        browser.implicitly_wait(15)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+@pytest.fixture()
+def browser():
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    browser.implicitly_wait(15)
 
     yield browser
     print("\nquit browser..")
